2f342a20d60110ad48bf5e6105aa4f9f6de4af31
Sumit Thigale <thigalesumit10@gmail.com>
2024-01-29 14:30:38 +0530

    commit

---
+++ Workbook/Challenge1.java
@@ -0,0 +1,14 @@
+public class Challenge1 { 
+        public static void main(String[] args) {
+    
+            System.out.println(" SSS     A  ");
+            System.out.println("S   S   A A ");
+            System.out.println("S      A   A");
+            System.out.println(" SSS   AAAAA");
+            System.out.println("    S  A   A");
+            System.out.println("S   S  A   A");
+            System.out.println(" SSS   A   A");
+
+        }
+
+}
