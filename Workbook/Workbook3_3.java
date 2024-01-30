b7f40e9ea36a4323f8cc7cd65bda16c3c51a0fb7
Sumit Thigale <98476747+sumitt10@users.noreply.github.com>
2024-01-30 14:51:39 +0530

    Workbook solution java-boot camp

---
+++ Workbook/Workbook3_3.java
@@ -0,0 +1,17 @@
+public class Workbook3_3 {
+    public static void main(String[] args) {
+
+        int temp = 25;
+
+        String forecast;
+        if (temp <= -1) {
+            forecast = "The forecast is FREEZING! Stay home!";
+        } else if (temp <= 10) {
+            forecast = "The forecast is Chilly. Wear a coat!";
+        } else {
+            forecast = "It's warm. go outside!";  
+        }
+
+        System.out.println(forecast);
+    }
+}
