e64e3f5a6f03183d5bb12135c0b0e9515fb4ac77
Sumit Thigale <98476747+sumitt10@users.noreply.github.com>
2024-01-31 22:48:24 +0530

    Example Related to HashMap

---
+++ Examples - JAVA Concepts/Examples4.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Examples4 {
+	static void printFreq(int arr[])
+	{
+	HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < arr.length; i++)
+		{
+			Integer c = hmap.get(arr[i]);
+
+			if (hmap.get(arr[i]) == null)
+			 {  hmap.put(arr[i], 1); }
+
+			else {
+			hmap.put(arr[i], ++c); }
+		}
+
+		for (Map.Entry m:hmap.entrySet())
+		System.out.println("Frequency of " + m.getKey() + 
+							" is " + m.getValue());
+	}
+
+	public static void main (String[] args)
+	{
+		int arr[] = {10, 34, 5, 10, 3, 5, 10};
+		printFreq(arr);
+	}
+}
