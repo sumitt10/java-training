f9ec475219c5bc0a03a77916f75a5f971e8a536f
Sumit Thigale <98476747+sumitt10@users.noreply.github.com>
2024-02-03 22:17:05 +0530

    Learn about a Hashcodes

---
+++ Examples - JAVA Concepts/Hashcode.java
@@ -0,0 +1,27 @@
+package com.example.examples;
+
+import java.util.Objects;
+
+public class Hashcode {
+	private String name;
+	private int age;
+
+	public Hashcode(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, age);
+	}
+
+	public static void main(String[] args) {
+		Hashcode person1 = new Hashcode("Alice", 25);
+		Hashcode person2 = new Hashcode("Bob", 30);
+
+		System.out.println("HashCode for person1: " + person1.hashCode());
+		System.out.println("HashCode for person2: " + person2.hashCode());
+
+	}
+}
